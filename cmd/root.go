ebe5010dc1034c344b1a569e11bb209b071dcfed
Han Joker <540090808@qq.com>
2024-12-19 00:41:56 +0800

    add init command

---
+++ cmd/root.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2024 韩忠康 <hun9k.github.io>
+The MIT License (MIT)
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "moo",
+	Short: "Go语言API脚手架工具",
+	Long:  "Go语言API脚手架工具，用于快速构建基于Go语言的后端API应用程序。",
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.moo.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 韩忠康 <hun9k.github.io>
+The MIT License (MIT)
+*/
+package main
+
+import "github.com/han-joker/moo/cmd"
+
+func main() {
+	cmd.Execute()
+}
